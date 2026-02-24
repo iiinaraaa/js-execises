3bfc96085f48d0ee55d19ea659725e6c1f73e35e
inara <inaraharu01@gmail.com>
2026-02-24 15:28:12 -0300

    interactive system

---
+++ ex/002-interactive-system/main.js
@@ -0,0 +1,17 @@
+alert("bem vind@ ⸜(｡˃ ᵕ ˂ )⸝♡");
+
+let name = prompt("qual seu nome?");
+alert("prazer," + name + "! (っ˶ ˘ ᵕ˘)ˆᵕ ˆ˶ς)");
+
+let choice = confirm("voce esta gostando de estudar js? ヾ(๑╹◡╹)ﾉ🔪")
+
+if (choice) {
+    alert("ebaa, eu tambem >⩊<");
+} else {
+    alert("que pena, eu to •ᴗ•");
+}
+
+let age = prompt("quantos anos voce tem? (👁,‿,👁)");
+alert("entao voce tem" + age + "anos!")
+
+alert("fim do programinha interativo ⸜(｡˃ ᵕ ˂ )⸝♡")
