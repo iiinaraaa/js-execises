37ec079d59253c1c6a000e3574c3aa738f0596d0
inara <inaraharu01@gmail.com>
2026-02-26 16:45:50 -0300

    making addition program

---
+++ ex/003-adding-values/main.js
@@ -0,0 +1,34 @@
+alert("vamos somar numeros... mas vai dar errado ¯\_(ツ)_/¯");
+
+let n1 = prompt("digite um numero");
+let n2 = prompt("digite outro numero");
+
+let s = n1 + n2 
+
+alert("soma dos valores:" + s)
+
+// pq isso acontece?
+// pq ele e como se fosse string, entao ele so junta
+
+alert("AGORA VAMOS SOMAR DE VDD ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧")
+
+let n3 = Number("1");
+let n4 = Number("2");
+let v = n3 + n4
+
+alert("valor da soma" + v);
+
+alert("ops errei, vou fzr dnv (ó﹏ò｡)");
+
+//REAL OFICIAL - INTERATIVO
+
+let n5 = +prompt("digite um numero");
+let n6 = +prompt("digite ouuutro numero");
+
+let e = n5 + n6
+
+alert("valor" + e + "( ദ്ദി ˙ᗜ˙ )");
+
+//meu deus deu certo, usei a logica e fiz gambiarra pra mostrar o valor desse ultimo ai 
+
+
