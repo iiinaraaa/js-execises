97431c17ea3e3c867dfb4a674aa0e14ccb9d6827
inara <inaraharu01@gmail.com>
2026-02-27 14:02:46 -0300

    fix code

--- ex/002-interactive-system/main.js
+++ ex/002-interactive-system/main.js
@@ -1,7 +1,7 @@
 alert("bem vind@ ⸜(｡˃ ᵕ ˂ )⸝♡");
 
 let name = prompt("qual seu nome?");
-alert("prazer," + name + "! (っ˶ ˘ ᵕ˘)ˆᵕ ˆ˶ς)");
+alert("prazer, " + name + " ! (っ˶ ˘ ᵕ˘)ˆᵕ ˆ˶ς)");
 
 let choice = confirm("voce esta gostando de estudar js? ヾ(๑╹◡╹)ﾉ🔪")
 
--- ex/003-adding-values/main.js
+++ ex/003-adding-values/main.js
@@ -1,33 +1,33 @@
 alert("vamos somar numeros... mas vai dar errado ¯\_(ツ)_/¯");
 
-let n1 = prompt("digite um numero");
-let n2 = prompt("digite outro numero");
+let input_1 = prompt("digite um numero");
+let input_2 = prompt("digite outro numero");
 
-let s = n1 + n2 
+let result_1 = input_1 + input_2 
 
-alert("soma dos valores:" + s)
+alert("soma dos valores: " + result_1)
 
 // pq isso acontece?
 // pq ele e como se fosse string, entao ele so junta
 
 alert("AGORA VAMOS SOMAR DE VDD ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧")
 
-let n3 = Number("1");
-let n4 = Number("2");
-let v = n3 + n4
+let input_3 = Number("1");
+let input_4 = Number("2");
+let result_2 = input_3 + input_4
 
-alert("valor da soma" + v);
+alert("valor da soma " + result_2);
 
 alert("ops errei, vou fzr dnv (ó﹏ò｡)");
 
 //REAL OFICIAL - INTERATIVO
 
-let n5 = +prompt("digite um numero");
-let n6 = +prompt("digite ouuutro numero");
+let input_5 = +prompt("digite um numero");
+let input_6 = +prompt("digite ouuutro numero");
 
-let e = n5 + n6
+let result_3 = input_5 + input_6
 
-alert("valor" + e + "( ദ്ദി ˙ᗜ˙ )");
+alert("valor " + result_3 + " ( ദ്ദി ˙ᗜ˙ )");
 
 //meu deus deu certo, usei a logica e fiz gambiarra pra mostrar o valor desse ultimo ai 
 
