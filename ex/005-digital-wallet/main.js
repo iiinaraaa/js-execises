9087251830b9f232da4346c6b536ef9814ce979f
inara <inaraharu01@gmail.com>
2026-03-30 17:09:18 -0300

    WIP - moved car example to another exercise folder

--- ex/005-digital-wallet/main.js
+++ ex/005-digital-wallet/main.js
@@ -1,7 +1,29 @@
-let saldo = 0
-let lazer = 0
-let pet = 0
-let faculdade = 0
+let conta_do_banco = {
+    "conta_corrente" : {
+        "nome": "Conta Corrente",
+        "saldo": 0,
+        "historico": []
+    },
+    "lazer" :{
+        "nome": "Caixinha - Lazer",
+        "saldo": 0,
+        "historico": []
+    },
+    "pet": {
+        "nome": "Caixinha - Pet",
+        "saldo": 0,
+        "historico": []
+    },
+    "faculdade": {
+        "nome": "Caixinha - Faculdade",
+        "saldo": 0,
+        "historico": []
+    },
+}
+
+console.log(">>>>>>>>>>>> conta_do_banco", conta_do_banco);
+// conta_do_banco["conta_corrente"].saldo += 10;
+console.log(">>>>>>>>>>>> conta_do_banco", conta_do_banco);
 
 function valorValido(valor) {
     if (isNaN(valor) || valor <= 0) {
@@ -16,8 +38,14 @@ function adicionarDinheiro() {
 
     if (!valorValido(valor)) return
 
-    saldo += valor
-    document.getElementById("saldo").innerText = saldo
+    conta_do_banco.conta_corrente.saldo += valor
+    conta_do_banco.conta_corrente.historico.push(valor);
+    document.getElementById("conta_corrente").innerText = conta_do_banco.conta_corrente.saldo
+
+    document.getElementById("conta_corrente_historico").innerHTML = ""    
+    conta_do_banco.conta_corrente.historico.map((entrada) => {
+        document.getElementById("conta_corrente_historico").innerHTML = document.getElementById("conta_corrente_historico").innerHTML + ` <p class="historico">Entrada: ${entrada}</p>`
+    })
 }
 
 function separar(caixinha) {
@@ -25,54 +53,54 @@ function separar(caixinha) {
 
     if (!valorValido(valor)) return
 
-    if (valor > saldo) {
+    if (valor > conta_do_banco.conta_corrente.saldo) {
         alert("voce nao tem dinheiro suficiente!")
         return
     }
 
-    saldo = saldo - valor
+    conta_do_banco.conta_corrente.saldo -= valor
 
     //se usa switch quando tem varias coisas de uma mesma coisa! nesse caso a caixinha tem 3 coisas
     //isso muda dependendo de algo? sim: switch, nao: fora/sem switch
     switch (caixinha) {
         case "lazer":
-            lazer += valor
-            document.getElementById("lazer").innerText = lazer
+            conta_do_banco.lazer.saldo += valor
+            document.getElementById("lazer").innerText = conta_do_banco.lazer.saldo
             break
 
         case "pet":
-            pet += valor
-            document.getElementById("pet").innerText = pet
+            conta_do_banco.pet.saldo += valor
+            document.getElementById("pet").innerText = conta_do_banco.pet.saldo
             break
 
         case "faculdade":
-            faculdade += valor
-            document.getElementById("faculdade").innerText = faculdade
+            conta_do_banco.faculdade.saldo += valor
+            document.getElementById("faculdade").innerText = conta_do_banco.faculdade.saldo
             break
     }
 
-    document.getElementById("saldo").innerText = saldo
+    document.getElementById("conta_corrente").innerText = conta_do_banco.conta_corrente.saldo
 }
 
 function render() {
-    if (lazer > 0) {
-        lazer = Number((lazer * 1.10).toFixed(2))
+    if (conta_do_banco.lazer.saldo > 0) {
+        conta_do_banco.lazer.saldo = Number((conta_do_banco.lazer.saldo * 1.10).toFixed(2))
         //manti dentro de number pra continuar sendo numero e n virar string com o tofixed
     }
 
-    if (pet > 0) {
-        pet = Number((pet * 1.10).toFixed(2))
+    if (conta_do_banco.pet.saldo > 0) {
+        conta_do_banco.pet.saldo = Number((conta_do_banco.pet.saldo * 1.10).toFixed(2))
     }
 
-    if (faculdade > 0) {
-        faculdade = Number((faculdade * 1.10).toFixed(2))
+    if (conta_do_banco.faculdade.saldo > 0) {
+        conta_do_banco.faculdade.saldo = Number((conta_do_banco.faculdade.saldo * 1.10).toFixed(2))
     }
 
-    document.getElementById("lazer").innerText = lazer
-    document.getElementById("pet").innerText = pet
-    document.getElementById("faculdade").innerText = faculdade
+    document.getElementById("lazer").innerText = conta_do_banco.lazer.saldo
+    document.getElementById("pet").innerText = conta_do_banco.pet.saldo
+    document.getElementById("faculdade").innerText = conta_do_banco.faculdade.saldo
 
-    if (lazer === 0 && pet === 0 && faculdade === 0) {
+    if (conta_do_banco.lazer.saldo === 0 && conta_do_banco.pet.saldo === 0 && conta_do_banco.faculdade.saldo === 0) {
         alert("saldo insuficiente para render, adicione mais dinheiro!")
     }
 }
@@ -89,30 +117,36 @@ function retirar(categoria) {
     if (!valorValido(valor)) return
 
     //add switch deixa mais legivel e menos repetitivo com if else if else if elseee
-    switch (categoria) {
-        case "lazer":
-            if (valor > lazer) return alert("saldo insuficiente, adicione mais dinheiro!!")
-            lazer -= valor
-            document.getElementById("lazer").innerText = lazer
-            break
+    // switch (categoria) {
+    //     case "lazer":
+    //         if (valor > conta_do_banco.lazer.saldo) return alert("saldo insuficiente, adicione mais dinheiro!!")
+    //         conta_do_banco.lazer.saldo -= valor
+    //         document.getElementById("lazer").innerText = conta_do_banco.lazer.saldo
+    //         break
 
-        case "pet":
-            if (valor > pet) return alert("saldo insuficiente, adicione mais dinheiro!!")
-            pet -= valor
-            document.getElementById("pet").innerText = pet
-            break
+    //     case "pet":
+    //         if (valor > conta_do_banco.pet.saldo) return alert("saldo insuficiente, adicione mais dinheiro!!")
+    //         conta_do_banco.pet.saldo -= valor
+    //         document.getElementById("pet").innerText = conta_do_banco.pet.saldo
+    //         break
 
-        case "faculdade":
-            if (valor > faculdade) return alert("saldo insuficiente, adicione mais dinheiro!!")
-            faculdade -= valor
-            document.getElementById("faculdade").innerText = faculdade
-            break
-            //guanabara disse pra sempre por break mesmo no final
-    }
+    //     case "faculdade":
+    //         if (valor > conta_do_banco.faculdade.saldo) return alert("saldo insuficiente, adicione mais dinheiro!!")
+    //         conta_do_banco.faculdade.saldo -= valor
+    //         document.getElementById("faculdade").innerText = conta_do_banco.faculdade.saldo
+    //         break
+    //         //guanabara disse pra sempre por break mesmo no final
+    // }
 
-    saldo += valor
+    if (valor > conta_do_banco[categoria].saldo) return alert("saldo insuficiente, adicione mais dinheiro!!")
+    conta_do_banco[categoria].saldo -= valor
+    document.getElementById(categoria).innerText = conta_do_banco[categoria].saldo
+
+
+
+    conta_do_banco.conta_corrente.saldo += valor
     //mesma coisa que saldo = saldo + valor!!!
-    document.getElementById("saldo").innerText = saldo
+    document.getElementById("conta_corrente").innerText = conta_do_banco.conta_corrente.saldo
 
     alert("dinheiro retirado com sucesso!!!")
 }
