7ba30ad2384db395e4017814495b7b8823c53248
inara <inaraharu01@gmail.com>
2026-05-28 17:07:02 -0300

    add bg and fix sourch

--- weather/main.js
+++ weather/main.js
@@ -1,4 +1,3 @@
-// api que eu criei la no site (nao sei quanto tempo ela dura)
 const API_KEY = "04dbd215797f4ee6b7c190852262105";
 
 const status = document.getElementById("status");
@@ -11,80 +10,85 @@ const uv = document.getElementById("uv");
 const time = document.getElementById("time");
 const mainIcon = document.getElementById("mainIcon");
 
-// custom dropdown
-const selectedCity = document.getElementById("selectedCity");
-const cityOptions = document.getElementById("cityOptions");
-const options = document.querySelectorAll(".weather__option");
+const weatherBackground = document.getElementById("weatherBackground");
 
-// codigos oficiais da weather api!!!!!!!
-// cada codigo vira um icon diferente
+const citySearch = document.getElementById("citySearch");
+const suggestions = document.getElementById("suggestions");
+
+let currentCity = "Florianopolis";
+
 const weatherIcons = {
-
     day: {
-
-        // ceu limpo
         1000: "sun.svg",
-
-        // nublado
         1003: "cloudy.svg",
         1006: "cloudy.svg",
         1009: "cloudy.svg",
-
-        // chuva leve
         1063: "rainyCloudyDay.svg",
-
-        // chuva
         1180: "rainCloud.svg",
         1183: "rainCloud.svg",
         1186: "rainCloud.svg",
         1189: "rainCloud.svg",
-
-        // neve
         1210: "snow.svg",
         1213: "snow.svg",
-
-        // tempestade
         1273: "thunderCloud.svg",
         1276: "thunderCloud.svg"
-
     },
 
     night: {
-
-        // ceu limpo
         1000: "moonStars.svg",
-
-        // nublado
         1003: "cloudMoonStars.svg",
         1006: "cloudMoonStars.svg",
         1009: "cloudMoonStars.svg",
-
-        // chuva leve
         1063: "rainyCloudyDay.svg",
-
-        // chuva
         1180: "rainCloud.svg",
         1183: "rainCloud.svg",
         1186: "rainCloud.svg",
         1189: "rainCloud.svg",
-
-        // neve
         1210: "snow.svg",
         1213: "snow.svg",
-
-        // tempestade
         1273: "thunderCloud.svg",
         1276: "thunderCloud.svg"
+    }
+};
 
+const weatherBackgrounds = {
+
+    day: {
+        1000: "sunnyDay.jpg",
+        1003: "cloudy.jpg",
+        1006: "cloudy.jpg",
+        1009: "cloudy.jpg",
+        1063: "rainDay.jpg",
+        1180: "rainDay.jpg",
+        1183: "rainDay.jpg",
+        1186: "rainDay.jpg",
+        1189: "rainDay.jpg",
+        1210: "rainDay.jpg",
+        1213: "rainDay.jpg",
+        1273: "rainDay.jpg",
+        1276: "rainDay.jpg"
+    },
+
+    night: {
+        1000: "night.jpg",
+        1003: "night.jpg",
+        1006: "night.jpg",
+        1009: "night.jpg",
+        1063: "rainNight.jpg",
+        1180: "rainNight.jpg",
+        1183: "rainNight.jpg",
+        1186: "rainNight.jpg",
+        1189: "rainNight.jpg",
+        1210: "rainNight.jpg",
+        1213: "rainNight.jpg",
+        1273: "rainNight.jpg",
+        1276: "rainNight.jpg"
     }
 
 };
 
-// function principal recebe o nome da cidade ai mostra os dados
-// async significa “essa função vai esperar informações da internet”
 async function getWeather(cityName) {
 
-    //fetch pede pra api os dados e dai mostra
     const response = await fetch(
         `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${cityName}&aqi=no`
     );
@@ -94,9 +98,7 @@ async function getWeather(cityName) {
     updateWeather(data);
 }
 
-// por se repetir nao seria melhor virar switch? mas sao coisas diferentes
 function updateWeather(data) {
-
     status.textContent = data.current.condition.text;
 
     temperature.textContent = `${data.current.temp_c.toFixed(0)} °C`;
@@ -109,33 +111,29 @@ function updateWeather(data) {
 
     uv.textContent = data.current.uv;
 
-    // pega codigo oficial da weather api
     const conditionCode = data.current.condition.code;
 
-    // verifica se eh dia ou noite
     const isDay = data.current.is_day;
 
-    // escolhe qual grupo usar
     const period = isDay ? "day" : "night";
 
-    // pega o icon correto
     const icon = weatherIcons[period][conditionCode];
 
-    // troca o icon principal automaticamente
-    // se nao encontrar nenhum icon usa cloudy
     mainIcon.src = `assets/img/icons/weatherIcons/${icon || "cloudy.svg"}`;
 
-    // pega data e hora da cidade pesquisada pela api
+    const bgImage = weatherBackgrounds[period][conditionCode];
+
+    const selectedBg = bgImage || (isDay ? "sunnyDay.jpg" : "night.jpg");
+
+    weatherBackground.style.backgroundImage = `url('assets/img/bg/${selectedBg}')`;
+
     const localTime = new Date(data.location.localtime);
 
     const formattedDate = localTime.toLocaleDateString("pt-BR");
 
     const formattedTime = localTime.toLocaleTimeString("pt-BR", {
-
-        //isso aq faz ter dois digitos, tipo 5, vai ficar 05
         hour: "2-digit",
         minute: "2-digit"
-
     });
 
     date.textContent = `${formattedDate} ${formattedTime}`;
@@ -150,49 +148,92 @@ function updateWeather(data) {
     `;
 }
 
-// abre e fecha dropdown
-selectedCity.addEventListener("click", () => {
+async function searchCities(query) {
+    if (query.length < 2) {
 
-    cityOptions.classList.toggle("active");
+        suggestions.innerHTML = "";
+        suggestions.classList.remove("active");
 
-});
-
-// quando clicar numa cidade
-options.forEach(option => {
-
-    option.addEventListener("click", () => {
-
-        const cityName = option.dataset.city;
-
-        selectedCity.querySelector(".weather__selectedText").textContent = cityName;
-
-        getWeather(cityName);
-
-        cityOptions.classList.remove("active");
-
-    });
-
-});
-
-// fecha se clicar fora
-window.addEventListener("click", (event) => {
-
-    if (!event.target.closest(".weather__customSelect")) {
-
-        cityOptions.classList.remove("active");
-
+        return;
     }
 
-});
-
-//aqui faz o clima aparecer automaticamente, pq se nao so vai aparecer algo quando a pessoa escolher
-getWeather("Florianopolis");
-
-// atualiza automaticamente a cada 1 minuto
-setInterval(() => {
-
-    getWeather(
-        selectedCity.querySelector(".weather__selectedText").textContent
+    const response = await fetch(
+        `https://api.weatherapi.com/v1/search.json?key=${API_KEY}&q=${query}`
     );
 
+    const data = await response.json();
+
+    suggestions.innerHTML = "";
+
+    suggestions.classList.add("active");
+
+    data.forEach(city => {
+        const div = document.createElement("div");
+
+        div.classList.add("weather__option");
+
+        div.textContent = `${city.name}, ${city.country}`;
+
+        div.addEventListener("click", () => {
+            currentCity = city.name;
+
+            citySearch.value = city.name;
+
+            getWeather(city.name);
+
+            suggestions.innerHTML = "";
+
+            suggestions.classList.remove("active");
+        });
+
+        suggestions.appendChild(div);
+    });
+
+}
+
+citySearch.addEventListener("input", () => {
+    searchCities(citySearch.value);
+});
+
+citySearch.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+
+        currentCity = citySearch.value;
+
+        getWeather(citySearch.value);
+
+        suggestions.classList.remove("active");
+    }
+});
+
+citySearch.addEventListener("focus", () => {
+    if (citySearch.value === currentCity) {
+
+        citySearch.value = "";
+
+    }
+});
+
+citySearch.addEventListener("blur", () => {
+    if (citySearch.value.trim() === "") {
+
+        citySearch.value = currentCity;
+
+    }
+});
+
+window.addEventListener("click", (event) => {
+    if (!event.target.closest(".weather__searchWrapper")) {
+
+        suggestions.classList.remove("active");
+
+    }
+});
+
+citySearch.value = currentCity;
+
+getWeather(currentCity);
+
+setInterval(() => {
+    getWeather(currentCity);
 }, 60000);
